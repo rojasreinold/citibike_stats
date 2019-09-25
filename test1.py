9c460770b6fa9191d1a8f2c9825f0d17e222eee7
rojasreinold <rojasreinold@gmail.com>
2019-09-24 22:28:04 -0400

    initial testing

---
+++ test1.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import pprint
+from gbfs.services import SystemDiscoveryService
+
+ds = SystemDiscoveryService()
+client = ds.instantiate_client('NYC')
+all_station_information = client.request_feed('station_information')
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(all_station_information)
