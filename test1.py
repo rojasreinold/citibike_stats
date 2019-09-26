80c0947b0f60f6ef13857982914912e36c62b88c
rojasreinold <rojasreinold@gmail.com>
2019-09-26 02:11:14 -0400

    Doing some basic tests

--- test1.py
+++ test1.py
@@ -1,9 +1,33 @@
 import pprint
-from gbfs.services import SystemDiscoveryService
+from gbfs.client import GBFSClient
 
-ds = SystemDiscoveryService()
-client = ds.instantiate_client('NYC')
-all_station_information = client.request_feed('station_information')
+def main():
+  client = GBFSClient('https://gbfs.citibikenyc.com/gbfs/gbfs.json', 'en')
+  station_names = all_stations_basic_info(client)
 
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(all_station_information)
+  pp = pprint.PrettyPrinter(indent=2)
+  #pp.pprint(all_station_information)
+  #print(stations_information)
+  pp.pprint(station_names)
+
+def live_status_for(station):
+  all_statuses = client.request_feed('station_status').get('data').get('stations')
+  return next(filter(lambda x: x.get('station_id') == station.get('station_id'), all_statuses))
+
+
+def all_stations_basic_info(client):
+  all_station_information = client.request_feed('station_information')
+  stations_list = all_station_information.get('data').get('stations') 
+  station_list = []
+  current_station = []
+  for station_all_info in stations_list :
+      current_station = []
+      current_station.append(station_all_info['station_id'])
+      current_station.append(station_all_info['name'])
+      station_list.append(current_station)
+  return station_list
+
+if __name__ == "__main__":
+  main()
+      
+
